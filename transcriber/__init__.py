12468e5ea0f77682f3fd0e2fc3e0f5ed2628e78d
Dexter Barney <dextergbarney@gmail.com>
2023-04-27 11:27:52 -0700

    Working on file upload functionality

--- transcriber/__init__.py
+++ transcriber/__init__.py
@@ -9,7 +9,7 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'transcriber.sqlite'),
-        UPLOAD_FOLDER=os.path.join(app.instance_path, 'transcriber.uploads')
+        UPLOADED_FILES=os.path.join(app.instance_path, 'files')
     )
 
     if test_config is None:
@@ -34,6 +34,7 @@ def create_app(test_config=None):
     from . import transcriber
     app.register_blueprint(transcriber.bp)
     app.add_url_rule('/', endpoint='index')
-    #app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    #app.config['UPLOAD_FOLDER'] = 'instance/UPLOAD_FOLDER'
+    #app.config['ALLOWED_EXTENSIONS'] = {'mp3', 'wav', 'aac', 'flac'}
 
     return app
--- transcriber/transcriber.py
+++ transcriber/transcriber.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, g, redirect, render_template, request, session, url_for,
 )
 from werkzeug.exceptions import abort
 from werkzeug.utils import secure_filename
@@ -8,9 +8,6 @@ from transcriber.auth import login_required
 from transcriber.db import get_db
 
 bp = Blueprint('transcriber', __name__)
-
-UPLOAD_FOLDER = '/instance/FILE_UPLOADS'
-ALLOWED_EXTENSIONS = {'mp3', 'wav', 'aac', 'flac'}
 
 
 @bp.route('/', methods=('GET', 'POST'))
@@ -21,27 +18,20 @@ def index():
         return render_template('transcriber/index.html')
     # Else return upload
     else:
+        #files = os.listdir(app.config['UPLOAD_FOLDER'])
         return render_template('transcriber/upload.html')
 
 
 @bp.route('/upload', methods=('GET', 'POST'))
 def upload():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
         file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('download_file', name=filename))
-        return render_template('transcriber/transcript.html')
+        if file.filename != '':
+            file.save(file.filename)
+            return 'file uploaded successfully'
+
+
+    return render_template('transcriber/transcript.html')
 
 
 @bp.route('/transcript', methods=('GET', 'POST'))
