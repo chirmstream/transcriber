e1ae081c73f1bc84193dfed6c6c666334b72501a
Dexter Barney <dextergbarney@gmail.com>
2023-04-27 10:48:26 -0700

    added transcipt render_template

--- transcriber/transcriber.py
+++ transcriber/transcriber.py
@@ -26,11 +26,27 @@ def index():
 
 @bp.route('/upload', methods=('GET', 'POST'))
 def upload():
-
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
         return render_template('transcriber/transcript.html')
 
 
-
+@bp.route('/transcript', methods=('GET', 'POST'))
+def transcript():
+    return render_template('transcriber/transcript.html')
 
 def allowed_file(filename):
     return '.' in filename and \
